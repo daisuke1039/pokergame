2e686935836028cfaa1a77f83e3a7e1a2f508b77
hattori daisuke <h.daisuke1039@gmail.com>
2021-06-23 17:48:26 +0900

    Improve array processing

--- functions.js
+++ functions.js
@@ -5,7 +5,6 @@ const randomNumber = () => {
 }
 
 const valueImage = Number => {
-
   const numberImages = {
     1: '../image/image-1.png',
     2: '../image/image-2.png',
@@ -22,25 +21,24 @@ const valueImage = Number => {
     13: '../image/image-13.png',
   }
   return numberImages[Number]
-
 }
 
 const poker = () =>  {
 
   //ランダムで求めた数字を5コ同時に出す
-  let v_number = [];
+  let resultNumber = [];
   for (i = 0 ; i < 5 ; i++) {
-    v_number[i] = randomNumber();
+    resultNumber[i] = randomNumber();
   }
 
   //降順に並べる
-  v_number = v_number.sort(function(a,b) {return a-b;});
+  resultNumber = resultNumber.sort(function(a,b) {return a-b;});
   
-  //target配列の画像を変更
-  let target = ['target01' , 'target02' , 'target03' , 'target04' , 'target05'];
-  for (i = 0 ; i < 5 ; i++) {
-    document.getElementById(target[i]).src = valueImage(v_number[i]);
-  }
+  //targetの画像を変更
+  let eleTarget = document.querySelectorAll('.target');
+  eleTarget.forEach((item, index) => {
+    item.src = valueImage(resultNumber[index]);
+  });
 
   //文字初期化
   document.getElementById('result').innerHTML = '';
@@ -48,13 +46,13 @@ const poker = () =>  {
   //重なり合う数字を数える
   let counts = [0, 0, 0, 0, 0, 0 ,0, 0, 0, 0, 0, 0, 0, 0];
   for (i = 0 ; i <= 14 ; i++) {
-    counts[v_number[i]]++;
+    counts[resultNumber[i]]++;
   }
 
   //重なりあった数字の数の最大値
   let Max = Math.max(...counts);
   //最大の数と最小の数の差
-  let Gap = v_number[4] - v_number[0];
+  let Gap = resultNumber[4] - resultNumber[0];
   let pair = 0;
   for (i = 0 ; i <= 14 ; i++) {
     if (counts[i] >= 2) {
@@ -95,14 +93,9 @@ const poker = () =>  {
 
 }
 
-const reset = () => {
-  document.location.reload();
-}
-
 //クリックに応じて表示を変える
-let first = 10;
 const reloadBtn = document.getElementById('reload_btn');
-const resetBtn = document.getElementById('reset_btn');
+let first = 10;
 reloadBtn.addEventListener('click', () => {
   poker();
   document.getElementById('c_number').textContent = --first;
@@ -112,8 +105,9 @@ reloadBtn.addEventListener('click', () => {
 });
 
 //リセットする
+const resetBtn = document.getElementById('reset_btn');
 resetBtn.addEventListener('click', () => {
-  reset();
+  document.location.reload();
 });
 
 
